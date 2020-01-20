afc3008a22b105b3477d9c5814daef5f7407ed08
arati.panigrahi <arati.panigrahi@accenture.com>
2020-01-20 09:59:57 +0530

    Baseline created for the UniversalApp

---
+++ MyUniversalApp/MyUniversalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyUniversalApp
+//
+//  Created by arati.panigrahi on 20/01/20.
+//  Copyright © 2020 arati.panigrahi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
